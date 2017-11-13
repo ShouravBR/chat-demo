48eafd150847ac3b4cc9d7f068080c39c49f987c
Shourav Bin Rabbani <shouravbr@outlook.com>
2017-11-13 18:52:00 +0000

    upgraded

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var animals = require('./animals').animals;
+var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  var user = animals.splice(Math.floor(Math.random()*animals.length), 1);
+  users.push(user);
+  console.log(user+" connected!");
+  socket.emit('username', {user: user, users: users});
+  io.emit('user connect', user);
   socket.on('chat message', function(msg){
+    console.log("[chat msg] "+msg.user+": "+msg.text);
     io.emit('chat message', msg);
+  });
+  socket.on('started writing', function(msg){
+    console.log("[+writing] "+msg);
+    io.emit('started writing', msg);
+  });
+  socket.on('stopped writing', function(msg){
+    console.log("[-writing] "+msg);
+    io.emit('stopped writing', msg);
   });
 });
 
